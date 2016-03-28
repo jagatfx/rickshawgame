ec8ee944493d988c2b21b5edec830d8b297f9322
Jacob Madden <jacob.madden@gmail.com>
2016-03-28 19:03:31 -0400

    added basic intro menu

---
+++ prototype1/Assets/Scripts/LoadOnClick.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadOnClick : MonoBehaviour
+{
+
+    public GameObject loadingImage;
+
+    public void LoadScene(int level)
+    {
+        loadingImage.SetActive (true);
+        SceneManager.LoadScene (level);
+    }
+
+}
+
